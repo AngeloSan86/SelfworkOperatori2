01be177747c6ed5c7fe4810832132d47aba2e548
Angelo Sanfilippo <124434790+AngeloSan86@users.noreply.github.com>
2024-07-04 15:50:21 +0200

    Add files via upload

---
+++ Selfwork Operatori 2/main.js
@@ -0,0 +1,7 @@
+let annoCorrente = 2024;
+const annoDiNascita = 1986;
+
+let eta = annoCorrente - annoDiNascita;
+let anniRimanenti = 100 - eta;
+
+console.log(`In quest momento siamo nell'anno ${annoCorrente}, tutti i nati nell'anno ${annoDiNascita} attualmente hanno ${eta} anni. Fra esattamente ${anniRimanenti} raggiungeranno i 100 anni!`);
